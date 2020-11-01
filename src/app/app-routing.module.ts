b9fe4ebc518536cdf97e72d4349eb1653251a994
Gabriel Mejia <endermejia032@gmail.com>
2020-11-01 21:58:45 +0100

    generate components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {LifestyleComponent} from "./components/lifestyle/lifestyle.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile', component: ProfileComponent
+  },
+  {
+    path: 'lifestyle', component: LifestyleComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: '', component: ProfileComponent
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {LOCALE_ID} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+import {FooterComponent} from './components/shared/footer/footer.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {LifestyleComponent} from './components/lifestyle/lifestyle.component';
+import {ContactComponent} from './components/contact/contact.component';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent,
+    ProfileComponent,
+    LifestyleComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
