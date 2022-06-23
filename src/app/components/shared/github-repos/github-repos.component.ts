4dc162429e74f57318b61767cde7172a6a722ee2
Gabriel Mejia <endermejia032@gmail.com>
2022-06-23 11:45:00 +0200

    FEATURE: github repos

--- src/app/components/shared/github-repos/github-repos.component.ts
+++ src/app/components/shared/github-repos/github-repos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GithubReposModel} from "../../../models/github-repos.model";
 
 @Component({
   selector: 'app-github-repos',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubReposComponent implements OnInit {
 
-  constructor() { }
+  public githubRepos: GithubReposModel = {
+    title: '🔥 My repositories',
+    show: true,
+    repos: [
+      'https://github-readme-stats.vercel.app/api/pin/?username=endermejia&repo=frontendCV',
+      'https://github-readme-stats.vercel.app/api/pin/?username=endermejia&repo=clubescaladacostablanca',
+      'https://github-readme-stats.vercel.app/api/pin/?username=endermejia&repo=ctrlThings',
+      'https://github-readme-stats.vercel.app/api/pin/?username=endermejia&repo=nobelApp'
+    ]
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/github-repos.model.ts
@@ -0,0 +1,5 @@
+export interface GithubReposModel {
+  title: string;
+  show: boolean;
+  repos: string[];
+}
