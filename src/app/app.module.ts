fb1688659e00ea0e251994568e8a6180880c9efe
Gabriel Mejia <endermejia032@gmail.com>
2023-07-29 08:46:36 +0200

    FIX: bug error formulario contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,31 @@ import {LOCALE_ID} from '@angular/core';
 import {registerLocaleData} from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 
-import {ContactComponent} from './components/contact/contact.component';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {CartComponent} from './components/cart/cart.component';
+import {ContactComponent} from './components/contact/contact.component';
 import {FooterComponent} from './components/shared/footer/footer.component';
 import {NavbarComponent} from './components/shared/navbar/navbar.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 import {ProductsComponent} from './components/products/products.component';
-import {FormsModule} from '@angular/forms';
 
 registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    ContactComponent,
     AppComponent,
     CartComponent,
+    ContactComponent,
     FooterComponent,
     NavbarComponent,
     PageNotFoundComponent,
     ProductsComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [
     {
       provide: LOCALE_ID,
